0fa63a779f6996006af357522b5d28f56310b1df
Vlad <armagiddon19@gmail.com>
2022-09-28 11:39:02 +0300

    use {} in refC

--- core/reactiveComponent/index.js
+++ core/reactiveComponent/index.js
@@ -2,12 +2,22 @@ import Type from "../type/proxy";
 
 import builder from "../core/builder";
 import { createNodeRebuild } from "../core/mount/rebiuld"
+import { typeOf } from "../core/helper";
 
 const s = (component) => {
+  let comp = component;
+  const type = typeOf(component);
+  if (type !== "function") {
+    if (type === "object") {
+      comp = () => component;
+    }
+  }
+
   const object = {
     parent: [],
-    value: component,
+    value: comp,
   }
+  console.log(object);
 
   return new Proxy(object, {
     get(target, prop) {
@@ -19,11 +29,21 @@ const s = (component) => {
     },
     set(target, prop, value) {
       if (prop in target) {
+        let comp = value;
+        if (prop === "value") {
+          const type = typeOf(component);
+          if (type !== "function") {
+            if (type === "object") {
+              comp = () => value;
+            }
+          }
+        }
+
         target[prop] = value;
 
         if (prop === "value") {
           if (target.parent.length > 0) {
-            let newObj = builder(value);
+            let newObj = builder(comp);
             const object  = createNodeRebuild(null, newObj);
             target.parent = target.parent.map((el) => {
               el.insertAdjacentElement('afterend', object);
